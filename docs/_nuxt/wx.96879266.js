3715f0f60f4e03e0ec0398b61c7afc5a16a5a537
张奇 <fugue620@gmail.com>
2025-05-09 11:15:12 +0800

    'update'

--- docs/_nuxt/wx.96879266.js
+++
@@ -1,1 +0,0 @@
-import"./entry.0c2f0be5.js";const o=""+globalThis.__publicAssetsURL("qr/wx.jpg");export{o as _};
---
+++ docs/nuxt_assets/wx.0e8d4a78.js
@@ -0,0 +1,1 @@
+import"./entry.8956cf88.js";const o=""+globalThis.__publicAssetsURL("qr/wx.jpg");export{o as _};
